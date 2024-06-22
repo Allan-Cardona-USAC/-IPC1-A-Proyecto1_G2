ab761280af299b092591f0d35f5e02f72f3ce680
Raúl Yat <3535811510101@ingenieria.usac.edu.gt>
2024-06-21 20:29:42 -0600

    frontend y pagina "Iniciar Sesión"
    El cuadro de los campos de inicio de sesión están mal

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Fragment } from 'react';
+import Router from './Components/Router/Router';
+
+function App() {
+  return (
+    // Importamos el enrutador el cual se encarga de manejar las rutas
+    <Fragment>
+      <Router />
+    </Fragment>
+  );
+}
+
+export default App;
