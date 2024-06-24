5b8f34c52d9f787caa02e2cdfbb2eb9df2b12890
Allan cardona <3004995740101@ingenieria.usac.edu.gt>
2024-06-24 16:01:35 -0600

    Funcionalidades de Admin
    Finalización de funcionalidades del admin

--- Backend/index.js
+++ Backend/index.js
@@ -12,6 +12,12 @@ const app = express();
 const PORT = 5000;
 // Nombre del archivo que nos dará persistencia de datos
 const FILENAME = 'Usuarios.json';
+//Nombre del archivo que nos dará persistencia de datos
+const FILENAMEMOVIE = 'Peliculas.json';
+//Nombre del archivo que nos dará persistencia de datos
+const FILENAMECOMMENT = 'Comentarios.json';
+//Nombre del archivo que nos dará persistencia de datos
+const FILENAMERECORD= 'Historial.json';
 
 /*//////////////////////----Especificaión del Framework------//////////////////////////// */
 // Le especificamos al framework que se usara el parseo de tipo json y los cors
@@ -21,22 +27,48 @@ app.use(cors());
 // Base de datos de Usuarios
 let dataUser = [];
 
-// Base de datos de Usuarios
+// Base de datos de Peliculas
 let dataMovie = [];
+
+// Base de datos de Reseñas
+let dataComment = [];
 
 // Verificar y crear archivo si no existe
 if (!fs.existsSync(FILENAME)) {
+    // Si el archivo no existe, crearlo con un array vacio 
+    //usando la funcion writeFileSync
     fs.writeFileSync(FILENAME, JSON.stringify(dataUser));
 } else {
     // Si el archivo existe, cargar los datos
     const fileData = fs.readFileSync(FILENAME, 'utf8');
     dataUser = JSON.parse(fileData);
 }
+
 // Función que ayuda a actualizar el contenido del archivo json
 function updateDataFile() {
     //vamos a sobreescribir el archivo con la nueva informacion
     fs.writeFileSync(FILENAME, JSON.stringify(dataUser));
 }
+
+
+// Verificar y crear archivo si no existe
+if (!fs.existsSync(FILENAMEMOVIE)) {
+    // Si el archivo no existe, crearlo con un array vacio
+    //usando la funcion writeFileSync
+    fs.writeFileSync(FILENAMEMOVIE, JSON.stringify(dataMovie));
+} else {
+    // Si el archivo existe, cargar los datos
+    const fileDataM = fs.readFileSync(FILENAMEMOVIE, 'utf8');
+    dataMovie = JSON.parse(fileDataM);
+
+}
+// Función que ayuda a actualizar el contenido del archivo json
+function updateDataFileM() {
+    //vamos a sobreescribir el archivo con la nueva informacion
+    fs.writeFileSync(FILENAMEMOVIE, JSON.stringify(dataMovie));
+}
+
+
 
 // Estructura para recibir peticiones:
 // app = variable con la que creamos nuestro backend haciendo uso del framework express
@@ -59,7 +91,7 @@ app.get('/', (req, res) => {
 app.get('/usuarios', (req, res) => {
     res.json(dataUser);
 });
-
+/*--------------------------------------------------------------------*/
 //get para peliculas
 app.get('/usuarios/peliculas', (req, res) => {
     let ver = {
@@ -68,7 +100,11 @@ app.get('/usuarios/peliculas', (req, res) => {
     };
     res.json(ver);
 });
-
+// Endpoint el cual retorna todas las Peliculas almacenados
+app.get('/usuarios/peliculas/catalogo', (req, res) => {
+    res.json(dataMovie);
+});
+/*--------------------------------------------------------------------*/
 
 // Endpoint el cual retorna un estudiante en especifico a partir de su correo
 // /:correo es un parametro el cual no conocemos su valor, es una manera dinamica de obtener valores
@@ -94,6 +130,7 @@ app.get('/usuarios/:correo', (req, res) => {
 
 
 /*/////////////////////------POST------//////////////////////////// */
+
 //Endpoint en el cual guardamos un nuevo usuario en la lista de usuarios,
 //la info se manda en el body en formato json
 app.post('/usuarios/registro', (req, res) => {
@@ -106,7 +143,20 @@ app.post('/usuarios/registro', (req, res) => {
     //status 2xx significa que la peticion fue exitosa
     res.status(201).send({response:'Usuario creado correctamente'});
 });
-
+/*--------------------------------------------------------------------*/
+//Endpoint en el cual guardamos una nueva pelicula en la lista de Peliculas,
+//la info se manda en el body en formato json
+app.post('/admin/registro/pelicula', (req, res) => {
+    // Guardamos el cuerpo de la peticion
+    const newMovie = req.body;
+    // Agregamos el estudiante a la lista
+    dataMovie.push(newMovie);
+    updateDataFileM();
+    // Brindamos un mensaje de confirmacion
+    //status 2xx significa que la peticion fue exitosa
+    res.status(201).send({response:'Pelicula Nueva creado correctamente'});
+});
+/*--------------------------------------------------------------------*/
 
 // Endpoint en el cual recibimos los datos del usuario que se quiere loggear, se valida si el usuario existe o no
 // en el array de usuarios y también se valida que su password sea correcto
@@ -137,6 +187,7 @@ app.post('/login', (req, res) => {
 
 
 /*/////////////////////------PUT------//////////////////////////// */
+
 // Endpoint con el cual actualizaremos los atributos de un objeto, menos su correo
 app.put('/usuarios/:correo', (req, res) => {
     // Obtenemos el correo del objeto que se va a actualizar
@@ -162,6 +213,40 @@ app.put('/usuarios/:correo', (req, res) => {
         res.status(202).send('Usuario actualizado correctamente');
     }
 });
+/*--------------------------------------------------------------------*/
+// Endpoint con el cual actualizaremos los atributos de un objeto, 
+// en este caso la pelicula sera Modificada completamente
+app.put('/admin/pelicula/:titulo', (req, res) => {
+    // Obtenemos el correo del objeto que se va a actualizar
+    const titulo = req.params.titulo;
+    // Obtenemos el cuerpo de la peticion en el cual vienen los nuevos valores
+    const updatedMovie = req.body;
+    // Obtenemos el indice de la lista en donde 
+    //se encuentra el objeto con el titulo especificado
+    const indexM = dataMovie.findIndex(admin => admin.titulo === titulo);
+    // Si no existe el objeto nos retorna un -1
+    //el -1 es una convencion para indicar que no se encontro el objeto
+    if (indexM === -1) {
+        //status 4xx significa que hubo un error del lado del cliente
+        res.status(404).send('Elemento no encontrado');
+    } else {
+        // Si se encontro el objeto, editamos sus atributos haciendo uso del indice
+        dataMovie[indexM].titulo = updatedMovie.titulo;
+        dataMovie[indexM].sinopsis = updatedMovie.sinopsis;
+        dataMovie[indexM].precio = updatedMovie.precio;
+        dataMovie[indexM].dierctor= updatedMovie.dierctor;
+        dataMovie[indexM].año= updatedMovie.año;
+        dataMovie[indexM].duracion= updatedMovie.duracion;
+        dataMovie[indexM].genero= updatedMovie.genero;
+        dataMovie[indexM].img= updatedMovie.img;
+
+        updateDataFileM();
+        //status 2xx significa que la peticion fue exitosa
+        res.status(202).send('Pelicula actualizado correctamente');
+    }
+});
+
+
 /*/////////////////////------DELETE------//////////////////////////// */
 // Endpoint con el cual eliminaremos un objeto de la lista de usuarios especifico
 app.delete('/usuarios/:correo', (req, res) => {
@@ -194,6 +279,41 @@ app.delete('/usuarios/:correo', (req, res) => {
         //status 2xx significa que la peticion fue exitosa
         res.status(200).send({ mensaje: 'Usuario eliminado correctamente' });
         console.log(`Usuario eliminado correctamente ${correo}`)
+    }
+});
+
+/*--------------------------------------------------------------------*/
+// Endpoint con el cual eliminaremos un objeto de la lista de pelicula especifico
+app.delete('/admin/pelicula/:titulo', (req, res) => {
+    // Obtenemosel correo del objeto que se va a actualizar
+    const titulo = req.params.titulo;
+    // Obtenemos el indice de la lista en donde se encuentra el objeto con el correo
+    const indexM = dataMovie.findIndex(peli => {
+        // Verificamos si el carnet del objeto es igual al correo que se quiere eliminar
+        console.log(peli.titulo)
+        if (peli.titulo === titulo) {
+            console.log("--------------------");
+            console.log("Pelicula encontrado");
+            console.log("--------------------");
+            
+            return peli
+            
+        }
+    });
+    // Si no existe el objeto nos retorna un -1
+    if (indexM === -1) {
+        //status 4xx significa que hubo un error del lado del cliente
+        res.status(404).send({ mensaje: 'Pelicula no encontrado' });
+        //else si existe el objeto
+    } else {
+        // Si se encontro el objeto, editamos sus atributos haciendo uso del indice
+        // Eliminamos el objeto
+        dataMovie.splice(indexM, 1);
+        // Actualizamos el archivo
+        updateDataFileM();
+        //status 2xx significa que la peticion fue exitosa
+        res.status(200).send({ mensaje: 'Pelicula Eliminada correctamente' });
+        console.log(`Pelicula eliminada correctamente: ${titulo}`)
     }
 });
 
